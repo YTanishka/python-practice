1747032477c8d9b8e911729fb32d6b2c6d03f792
YTanishka <tanishkayadav6000@gmail.com>
2026-08-06 18:11:18 +0530

    more-codes

--- intro.py
+++ intro.py
@@ -1,2 +1,22 @@
 language = "Python"
 print(language)
+
+age = 22
+print(type(age))
+
+print(20/4)
+print(20//4)
+
+number = int("15")
+print(number + 5)
+
+# str + number error deta h 
+# age = "20"
+# print(age + 5)
+
+#number = int("Python")
+
+price = int(input())
+quantity = 2
+total = price * quantity
+print(total)
