49094f3a909050174bcbb4d973aa96d375ad47ec
YTanishka <tanishkayadav6000@gmail.com>
2026-08-14 19:09:14 +0530

    type of input

--- intro.py
+++ intro.py
@@ -21,9 +21,13 @@ quantity = 2
 total = price * quantity
 print(total)
 
-age = "20"
-print(age + 5) #error deta h
+#age = "20"
+#print(age + 5) #error deta h
 
-number = int("Python") #error deta h
-print(number)
+#number = int("Python") #error deta h
+#print(number)
 
+price = int(input())
+quantity = 2 
+total = price * quantity
+print(f"Total: {total}")
---
+++ task3.py
@@ -0,0 +1,7 @@
+name = input()
+age = int(input())
+percentage = float(input())
+
+print(type(name))
+print(type(age))
+print(type(percentage))
